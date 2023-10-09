e5b90018b29046bc0f1ebf50b2b4c767298b0aa6
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-09 21:00:18 +0200

    added pull down force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
-    public float forwardspeed = 0.05f;
-    
+    public float forwardSpeed = 0.05f;
+    public float jumpForce = 200f;
+    public float pullDownForce = -10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,18 @@ public class PlayerController: MonoBehaviour
         if(Input.GetButtonDown("Jump"))
         {
             Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
-            rigidbody.AddForce(0, 300, 0);
+            rigidbody.AddForce(0, jumpForce, 0);
         }
-        transform.Translate(0,0,forwardspeed);
+        transform.Translate(0,0,forwardSpeed);
         
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (rigidbody.velocity.y < -.1f)
+        {
+            rigidbody.AddForce(0,pullDownForce,0);
+        }
     }
 }
